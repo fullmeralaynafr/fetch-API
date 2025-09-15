1ef06178d80b1caaf1d891c7a2dbdec10a100d2d
fullmeralaynafr <fullmeralaynafr@gmail.com>
2025-09-15 17:11:54 -0600

    v1

---
+++ script.js
@@ -0,0 +1,7 @@
+const url = "https://iskarr.github.io/austindonovan.github.io/api/business.json";
+
+fetch(url)
+.then((response) => response.json())
+.then((jsObject) => {
+console.log(jsObject);
+});
